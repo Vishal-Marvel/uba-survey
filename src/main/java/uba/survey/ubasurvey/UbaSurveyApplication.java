e8baf508627ee46cd1da4f70d1c291cd8dd4052f
Vishal-Marvel <vishal.pvn.edu@gmail.com>
2023-11-28 19:05:35 +0530

    init

---
+++ src/main/java/uba/survey/ubasurvey/UbaSurveyApplication.java
@@ -0,0 +1,13 @@
+package uba.survey.ubasurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbaSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbaSurveyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uba/survey/ubasurvey/UbaSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package uba.survey.ubasurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbaSurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
